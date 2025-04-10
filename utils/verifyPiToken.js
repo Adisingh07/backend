4807d25c3631491e424263e4bce940b694b52bf4
Adisingh07 <88798791+Adisingh07@users.noreply.github.com>
2025-04-11 03:25:11 +0530

    Update verifyPiToken.js

--- utils/verifyPiToken.js
+++ utils/verifyPiToken.js
@@ -1,12 +1,13 @@
-import dotenv from "dotenv";
-dotenv.config();
+const axios = require("axios");
 
 const verifyPiToken = async (token) => {
   const response = await axios.get(`https://api.minepi.com/me`, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "X-PI-API-KEY": process.env.PI_API_KEY,
+      "X-PI-API-KEY": process.env.PI_API_KEY, // if you're using API key
     },
   });
   return response.data;
 };
+
+module.exports = verifyPiToken;
